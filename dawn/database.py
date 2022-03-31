08da6d6c40b99631d76c4ba95c0eddd94073c392
Jartan <jgogox@gmail.com>
2022-03-30 21:15:01 -0400

    Added internal functionality & auto table initialization system for new dbs & setup db migration framework

--- dawn/database.py
+++ dawn/database.py
@@ -13,7 +13,8 @@ class Database:
     def __init__():
         pass
 
-    def create_database_file(self, path: str, name: str) -> None:
+    @classmethod
+    def create_database_file(cls, path: str, name: str) -> None:
         """
         Creates an empty database file
         Raises FileExistsError if file already exists
@@ -26,8 +27,46 @@ class Database:
         else:
             raise FileExistsError(file=db_path)
 
+    def _read_file(self, file: str) -> str:
+        with open(file, "r") as stream:
+            content = stream.read()
+        return content
+
+    async def _execute_script_from_file(self, file: str) -> None:
+        """
+        Opens and executes a the provided file as a command in another thread to prevent async blocking
+            file - path of the file to open
+        """
+        task = asyncio.create_task(asyncio.to_thread(self._read_file(file)))
+        script = await asyncio.wait(task)[0]
+        await self._conn.executescript(script)
+
     async def _create_connection(self) -> None:
         self._conn = await aiosqlite.connect(self.db_path)
+
+    async def _update(self) -> str:
+        """
+        Updates the database to the latest version
+        Returns the new version of the database
+        Returns None if no changes were made
+
+        """
+        prev_vers = self._conn.execute("PRAGMA user_version;")  # query version of db
+        version = prev_vers
+
+        # EXAMPLE:
+        # doing it like this we save space and just have it incrementally update through the versions rather than having to program for every combination
+
+        # if version == 1.0:
+        #     do stuff
+        #     version = 1.1 or whatever this version is changing to
+        # if version == 1.1:
+        #     do stuff
+        #     version = 1.2
+
+        if version == prev_vers:
+            return None
+        return version
 
     @classmethod
     async def startup(self, path: str, db_name: str, version: str) -> None:
@@ -53,16 +92,17 @@ class Database:
                     asyncio.to_thread(self.create_database_file(self.path, self.name))
                 )
                 await asyncio.gather(task)
-                create_tables = True
+                new_db = True
             except FileExistsError:
                 pass
 
         await self._create_connection()
 
-        if create_tables:
-            pass
-            # create tables here
-
-        # run update tasks here
+        if new_db:
+            await self._execute_script_from_file(
+                os.path.join(self.path, "schema.sql")
+            )  # Running schema setup
+        else:
+            await self._update()  # update tasks
 
         return self
