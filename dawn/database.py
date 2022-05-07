b5d90706e88aba9eddb56ad25f916c6098fa904b
Jartan <jgogox@gmail.com>
2022-05-07 17:44:54 -0400

    Added context manager accessability, fetch functions, reorganized, and other small changes

--- dawn/database.py
+++ dawn/database.py
@@ -1,4 +1,5 @@
 import asyncio
+from contextlib import asynccontextmanager
 import logging
 import os
 import sys
@@ -9,10 +10,63 @@ import aiosqlite
 log = logging.getLogger(__name__)
 
 
-class Database:
+class DatabaseManager:
     def __init__(self):
-        self.conn = property(self.get_conn)
+        self.conn = property(self._get_conn)  # getter function
         pass
+
+    @asynccontextmanager
+    async def __context_create(self, path: str, db_name: str, version: str):
+        """
+        Context manager version of DatabaseManager.create
+        """
+
+        await self.create(path, db_name, version)
+
+        try:
+            yield self
+        finally:
+            await self.close()
+
+    async def create(self, path: str, db_name: str, version: str) -> None:
+        """
+        Catch-all factory method that:
+            1. Creates a database file if necessary
+            2. Creates tables if necessary
+            3. Runs update tasks based on input version
+            4. Returns self
+
+            path - the location of the database file
+            db_name - the name of the database file
+            version - the version of the Database running (to do version upgrade changes to database)
+        """
+        self.path = path
+        self.name = db_name
+        self.db_path = os.path.join(path, f"{db_name}.db")
+        self.version = version
+
+        # creating database file if it doesn't exist
+        if not os.path.isfile(self.db_path):
+            try:
+                await self.create_database_file(self.path, self.name)
+                new_db = True
+            except FileExistsError:
+                pass
+
+        # creating connection
+        await self._create_connection()
+
+        if new_db:
+            # Running schema setup
+            await self._execute_script_from_file(os.path.join(self.path, "schema.sql"))
+        else:
+            # update tasks
+            await self._migrate()
+
+        return self
+
+    async def close(self) -> None:
+        await self._conn.close()
 
     @classmethod
     async def create_database_file(cls, path: str, name: str) -> None:
@@ -29,7 +83,9 @@ class Database:
         else:
             raise FileExistsError(file=db_path)
 
-    def _read_file(self, file: str) -> str:
+    def __read_file(self, file: str) -> str:
+        # comment below is possible improvement to remove repeated code, but need to test before implementing TODO
+        # asyncio.to_thread(content = open(file, "r").read())
         with open(file, "r") as stream:
             content = stream.read()
         return content
@@ -39,14 +95,37 @@ class Database:
         Opens and executes a the provided file as a command in another thread to prevent async blocking
             file - path of the file to open
         """
-        task = asyncio.create_task(asyncio.to_thread(self._read_file(file)))
+        task = asyncio.create_task(asyncio.to_thread(self.__read_file(file)))
         script = await asyncio.gather(task)[0]
-        await self._conn.executescript(script)
+        await self.conn.executescript(script)
 
     async def _create_connection(self) -> None:
         self._conn = await aiosqlite.connect(self.db_path)
 
-    def get_conn(self) -> aiosqlite.Connection:
+    async def fetchone(self, query: str):
+        """
+        Queries and fetches one matching line from the database
+            query - the query to execute on the database
+        """
+
+        cursor = await self.conn.execute(query)
+        row = await cursor.fetchone()
+        await cursor.close()
+        return row
+
+    async def fetchall(self, query: str):
+        """
+        Queries and fetches all matching lines from the database
+            query - the query to execute on the database
+        """
+
+        cursor = await self.conn.execute(query)
+        rows = await cursor.fetchall()
+        await cursor.close()
+        return rows
+
+    def _get_conn(self) -> aiosqlite.Connection:
+        # TODO add check that connection is active, and handle if not
         return self._conn
 
     async def _migrate(self) -> str:
@@ -56,7 +135,7 @@ class Database:
         Returns None if no changes were made
 
         """
-        prev_vers = self._conn.execute("PRAGMA user_version;")  # query version of db
+        prev_vers = self.conn.execute("PRAGMA user_version;")  # query version of db
         version = prev_vers
 
         # EXAMPLE:
@@ -72,39 +151,3 @@ class Database:
         if version == prev_vers:
             return None
         return version
-
-    @classmethod
-    async def startup(self, path: str, db_name: str, version: str) -> None:
-        """
-        Catch-all task that:
-            1. Creates a database file if necessary
-            2. Creates tables if necessary
-            3. Runs update tasks based on input version
-            4. Returns self
-
-            path - the location of the database file
-            db_name - the name of the database file
-            version - the version of Dawn running (used to perform update changes to database)
-        """
-        self.path = path
-        self.name = db_name
-        self.db_path = os.path.join(path, f"{db_name}.db")
-        self.version = version
-
-        if not os.path.isfile(self.db_path):
-            try:
-                await self.create_database_file(self.path, self.name)
-                new_db = True
-            except FileExistsError:
-                pass
-
-        await self._create_connection()
-
-        if new_db:
-            await self._execute_script_from_file(
-                os.path.join(self.path, "schema.sql")
-            )  # Running schema setup
-        else:
-            await self._migrate()  # update tasks
-
-        return self
